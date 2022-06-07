7cc009becf97cee367f0c036ccabbe8897eb04de
Bartłomiej Kubica <bartlomiej.kubica@ext.nordiska.com>
2022-06-07 09:01:03 +0200

    initial test for reservation service

---
+++ tests/MySpot.Tests.Unit/Services/ReservationServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySpot.Api.Commands;
+using Xunit;
+
+namespace MySpot.Tests.Unit.Services
+{
+    public class ReservationServiceTest
+    {
+        [Fact]
+        public void given_valid_command_create_should_add_reservation()
+        {
+            var id = Guid.NewGuid();
+            var command = new CreateReservation(id, Guid.NewGuid(), "Joe Doe", "XYZ123", DateTime.UtcNow);
+        }
+    }
+}
