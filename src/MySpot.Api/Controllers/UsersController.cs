5f5602f309b574409c84fb00061d5b6903740696
Bartłomiej Kubica <bartlomiej.kubica@ext.nordiska.com>
2022-07-12 11:25:30 +0200

    addeed methods to controller

--- src/MySpot.Api/Controllers/UsersController.cs
+++ src/MySpot.Api/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MySpot.Application.Abstractions;
 using MySpot.Application.Commands;
+using MySpot.Application.DTO;
+using MySpot.Application.Queries;
 
 namespace MySpot.Api.Controllers;
 
@@ -9,16 +11,38 @@ namespace MySpot.Api.Controllers;
 public class UsersController: ControllerBase
 {
     private readonly ICommandHandler<SignUp> _signUpCommandHandler;
+    private readonly IQueryHandler<GetUsers, IEnumerable<UserDto>> _getUsersHandler;
+    private readonly IQueryHandler<GetUser, UserDto> _getUserHandler;
 
-    public UsersController(ICommandHandler<SignUp> signUpCommandHandler)
+    public UsersController(ICommandHandler<SignUp> signUpCommandHandler,
+        IQueryHandler<GetUsers, IEnumerable<UserDto>> getUsersHandler,
+        IQueryHandler<GetUser, UserDto> getUserHandler)
     {
         _signUpCommandHandler = signUpCommandHandler;
+        _getUsersHandler = getUsersHandler;
+        _getUserHandler = getUserHandler;
     }
+
+    [HttpGet("{userId:guid}")]
+    public async Task<IActionResult> Get(Guid userId)
+    {
+        var user = await _getUserHandler.HandleAsync(new GetUser { UserId = userId });
+        
+        if (user == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(user);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get() => Ok(await _getUsersHandler.HandleAsync(new GetUsers()));
 
     [HttpPost]
     public async Task<ActionResult> Post(SignUp command)
     {
         await _signUpCommandHandler.HandleAsync(command with { UserId = Guid.NewGuid() });
-        return NoContent();
+        return CreatedAtAction(nameof(Get), new { userId = command.UserId }, null);
     }
 }
